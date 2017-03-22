e9c294847a359ca0d8a570329015959d49e402db
Unknown <salvadorcerdaholmager@enti.cat>
2017-03-22 20:22:53 +0100

    Movimiento de camara, jugador y rotación (ademas de un nuevo sprite para el Player)

---
+++ SubjectZero/Assets/Scripts/CamaraSeguir.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class CamaraSeguir : MonoBehaviour {
+    GameObject player;
+    bool followPlayer = true;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (followPlayer == true)
+            camFollowPlayer();
+	}
+    public void SetFollowPlayer(bool val)
+    {
+        followPlayer = val;
+    }
+    void camFollowPlayer()
+    {
+        Vector3 newPos = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+        this.transform.position = newPos;
+    }
+}
---
+++ SubjectZero/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+    public bool moviendo = false;
+    float velocidad = 8.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Mov();	
+	}
+
+    void Mov() {
+       
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * velocidad * Time.deltaTime, Space.World);
+            moviendo = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * velocidad * Time.deltaTime, Space.World);
+            moviendo = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * velocidad * Time.deltaTime, Space.World);
+            moviendo = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * velocidad * Time.deltaTime, Space.World);
+            moviendo = true;
+        }
+        if (Input.GetKey(KeyCode.S)!=true&& Input.GetKey(KeyCode.D) != true&& Input.GetKey(KeyCode.W) != true&& Input.GetKey(KeyCode.A) != true)
+        {
+            
+            moviendo = false;
+        }
+
+    }
+
+
+
+
+}
