70c5068110f298c4536204ff5cd6995e164c4c31
Unknown <magopru@hotmail.es>
2017-04-19 17:37:11 +0200

    Disparos progreso (Direccion, tiempo, colision, daño)

---
+++ SubjectZero/Assets/Scripts/Disparos.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparos : MonoBehaviour {
+
+    public GameObject proyectilPre;
+
+    private List<GameObject> Proyectiles = new List<GameObject>();
+
+    private float projectileVelocity;
+
+	// Use this for initialization
+	void Start () {
+        projectileVelocity = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = (GameObject)Instantiate(proyectilPre, transform.position, Quaternion.identity);
+            Proyectiles.Add(bullet);
+        }	
+
+        for(int i = 0; i < Proyectiles.Count; i++)
+        {
+            GameObject goBullet = Proyectiles[i];
+            if(goBullet != null)
+            {
+                goBullet.transform.Translate(new Vector3(0, 1) * Time.deltaTime * projectileVelocity);
+
+                Vector3 bulletScreenPos = Camera.main.WorldToScreenPoint(goBullet.transform.position);
+                if(bulletScreenPos.y >= Screen.height || bulletScreenPos.y <= 0)
+                {
+                    DestroyObject(goBullet);
+                    Proyectiles.Remove(goBullet);
+                }
+
+            }
+        }
+	}
+}
