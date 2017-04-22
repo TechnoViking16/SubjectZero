e80b52e90fb24b9516fa4e43f97c629be9ac8cca
Martí Gómez <magopru1995@gmail.com>
2017-04-22 02:00:08 +0200

    Y hay colision de balas. HAY QUE AÑADIR TAG A TODO (ENEMIGO; WALLS DOOR)

---
+++ SubjectZero/Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //OnCollisionEnter2D();
+        //OnTriggerEnter("InteriorWalls");
+    }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Wall" || col.gameObject.tag=="Door")
+        {
+            Destroy(gameObject);
+        }
+        if(col.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
