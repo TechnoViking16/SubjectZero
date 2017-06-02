9dc1a57578c1673923b1f3add562a56c5acaf00e
Javi <javiercamaragarcia@enti.cat>
2017-06-02 14:44:53 +0200

    Resubida BulletTime(Reflejos)
    1.2

--- SubjectZero/Assets/Scripts/BulletTimePlayer.cs
+++ SubjectZero/Assets/Scripts/BulletTimePlayer.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 public class BulletTimePlayer : MonoBehaviour
 {
 
-
+    
     public float currentSlowMo = 0.0f;
     public float slowTimeAllowed = 2.0f;
-
+    
 
     void Start() { }
 
     void Update()
     {
+        /*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 0.4f;
+            Time.fixedDeltaTime = 0.02F * Time.timeScale;
+        }
+        if (Time.timeScale == 0.4f)
+        {
+            currentSlowMo += Time.deltaTime;
+        }
 
+        if (currentSlowMo > slowTimeAllowed)
+        {
+            currentSlowMo = 0.0f;
+            Time.timeScale = 1.0f;
+        }
+    }
+    */
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (Time.timeScale == 1.0)
-
+            {
                 Time.timeScale = 0.4f;
-
+                Time.fixedDeltaTime = Time.timeScale * 0.02f;
+            }
             else
             {
                 Time.timeScale = 1.0f;
@@ -33,9 +51,9 @@ public class BulletTimePlayer : MonoBehaviour
 
             if (currentSlowMo > slowTimeAllowed)
             {
-                currentSlowMo = 0.0f; 
+                currentSlowMo = 0.0f;
                 Time.timeScale = 1.0f;
             }
         }
     }
-}
+ }   
